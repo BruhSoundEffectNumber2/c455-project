Computing derivatives and fixed points

iterate.py
@@ -2,11 +2,26 @@ import numpy as np
 
 BETA = 10
 
+def h(l: np.float64, x: np.float64) -> np.float64:
+    """Hassell's map function."""
+    return l * x / np.pow(1 + x, BETA)
+
+def hprime(l: np.float64, x: np.float64):
+    """Derivative of Hassell's map function."""
+    return l * (1 + x) ** (-BETA - 1) * (1 + x - BETA * x)
+
+def h_fixed_points(l: np.float64) -> np.float64:
+    """Returns the second fixed point of Hassell's map. Only valid for l>=1. At l=1, there is only one fixed point at 0."""
+    if l < 1:
+        return np.nan
+    
+    return np.power(l, 1 / BETA) - 1
+
 
 def iterate(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
     """Iterates Hassell's map the given number of times, returning the number of iterations and the array of iterates."""
     out = np.zeros(lim, dtype=np.float64)
-    x = l * x0 / np.pow(1 + x0, BETA)
+    x = h(l, x0)
     out[0] = x
 
     # Start at 1 because we've already done the first iteration with x0
@@ -19,7 +34,7 @@ def iterate(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
         if x > 100:
             return i, out
 
-        x = l * x / np.pow(1 + x, BETA)
+        x = h(l, x)
         out[i] = x
 
     return lim, out
@@ -27,7 +42,7 @@ def iterate(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
 def iterate_postrans(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
     """Iterates Hassell's map lim + 200 times, returning the number of iterations and the array of iterates after the first 200."""
     out = np.zeros(lim, dtype=np.float64)
-    x = l * x0 / np.pow(1 + x0, BETA)
+    x = h(l, x0)
 
     # Start at 1 because we've already done the first iteration with x0
     for i in range(1, lim + 200):
@@ -39,7 +54,7 @@ def iterate_postrans(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.n
         if x > 100:
             return i - 200, out
 
-        x = l * x / np.pow(1 + x, BETA)
+        x = h(l, x)
         if i >= 200:
             out[i - 200] = x
 