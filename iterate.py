d30dab8ae60d59a0735b2c3f85e9cfc9b9749073
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-04-15 08:36:25 -0700

    Fix beta as a constant

--- iterate.py
+++ iterate.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+BETA = 10
 
-def iterate(l: np.float64, b: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
+
+def iterate(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
     """Iterates Hassell's map the given number of times, returning the number of iterations and the array of iterates."""
     out = np.zeros(lim, dtype=np.float64)
-    x = l * x0 / np.pow(1 + x0, b)
+    x = l * x0 / np.pow(1 + x0, BETA)
     out[0] = x
 
     # Start at 1 because we've already done the first iteration with x0
@@ -17,15 +19,15 @@ def iterate(l: np.float64, b: np.float64, x0: np.float64, lim: int) -> tuple[int
         if x > 100:
             return i, out
 
-        x = l * x / np.pow(1 + x, b)
+        x = l * x / np.pow(1 + x, BETA)
         out[i] = x
 
     return lim, out
 
-def iterate_postrans(l: np.float64, b: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
+def iterate_postrans(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.ndarray]:
     """Iterates Hassell's map lim + 200 times, returning the number of iterations and the array of iterates after the first 200."""
     out = np.zeros(lim, dtype=np.float64)
-    x = l * x0 / np.pow(1 + x0, b)
+    x = l * x0 / np.pow(1 + x0, BETA)
 
     # Start at 1 because we've already done the first iteration with x0
     for i in range(1, lim + 200):
@@ -37,7 +39,7 @@ def iterate_postrans(l: np.float64, b: np.float64, x0: np.float64, lim: int) -> 
         if x > 100:
             return i - 200, out
 
-        x = l * x / np.pow(1 + x, b)
+        x = l * x / np.pow(1 + x, BETA)
         if i >= 200:
             out[i - 200] = x
 
