efd7b69133089ed768848ee8c67ab3ffb7e27303
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-04-22 13:10:20 -0700

    Initial work on figenbaum

--- iterate.py
+++ iterate.py
@@ -59,3 +59,28 @@ def iterate_postrans(l: np.float64, x0: np.float64, lim: int) -> tuple[int, np.n
             out[i - 200] = x
 
     return lim, out
+
+def find_super_stable(k: np.int64, l_guess: np.float64) -> tuple[np.int64, np.float64]:
+    n = 2 ** (k - 1)
+    l = l_guess
+
+    for i in range(1000):
+        x = np.float64(1.0) / (BETA - 1)
+        xp = np.float64(0)
+
+        for _ in range(n):
+            xp = x / np.pow((x + 1), BETA)
+            x = h(l, x)
+
+        corr = (x - 1 / (BETA - 1)) / xp
+        l -= corr
+
+        if not np.isfinite(l):
+            raise RuntimeError(f"l has become NaN or Inf at {i} iterations")
+
+        # print(l)
+
+        if abs(corr) < 1e-14:
+            return i + 1, l
+    
+    raise RuntimeError(f"Failed to find super stable point (last guess: {l})")
